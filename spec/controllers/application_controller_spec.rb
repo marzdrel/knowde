442f192df5123b1d45452325e29e06f4276922b7
Marz Drel <marzdrel@users.noreply.github.com>
2022-02-04 09:58:37 +0100

    Add basic spec for ApplicationController

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,4 +1,10 @@
 require "rails_helper"
 
 RSpec.describe ApplicationController, type: :controller do
+  describe "#action_args" do
+    it "returns expected args" do
+      expect(controller.action_args)
+        .to be_an Array
+    end
+  end
 end
